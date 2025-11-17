3ab7624f3382e9901fcd6dfdeb7626246ca57db6
berkano <gmoshtalyov@yandex.ru>
2025-11-17 22:10:11 +0300

    Первый коммит: добавлены исходные файлы

--- MicroserviceReader/MicroserviceReader/Models/Reader.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MicroserviceReader.Models;
-public class Reader
-{
-    public long Id { get; set; }
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public int BorrowedBooks { get; set; }
-}
