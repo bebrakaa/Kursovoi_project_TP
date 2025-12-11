d685acb94707897629792dd4f4dbcc113c994888
bebrakaa <yujmuytyu@gmail.com>
2025-12-11 17:17:54 +0300

    Удаление некорректного модуля с API

--- src/InsuranceAgency.Web/ProgramTestAccess.cs
+++
@@ -1,8 +0,0 @@
-// This file makes Program class accessible for integration tests
-// In .NET 8 with top-level statements, Program is implicitly internal
-// This partial class declaration makes it public for testing
-
-namespace InsuranceAgency.Web;
-
-public partial class Program { }
-
