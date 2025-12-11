0da20066816a6932f8c7114eae99b575250d106f
bebrakaa <yujmuytyu@gmail.com>
2025-12-11 18:00:08 +0300

    Обновление проекта

--- InsuranceAgency.Application/DTOs/Payment/PaymentResultDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InsuranceAgency.Application.DTOs.Payment
-{
-    public class PaymentResultDto
-    {
-        public bool Success { get; set; }
-        public string? Error { get; set; }
-        public string? TransactionId { get; set; }
-    }
-}
--- InsuranceAgency.Application/Interfaces/Services/INotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace InsuranceAgency.Application.Interfaces.Services
-{
-    public interface INotificationService
-    {
-        Task SendAsync(string email, string subject, string body);
-    }
-}
